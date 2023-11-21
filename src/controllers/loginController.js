97671c0409d0fde0b97b2954beff5001bac7d3d6
FernandoGutemberg <fernando70sil@gmail.com>
2023-11-21 01:42:08 -0300

    e-mail e senha invalidos
    16:57

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,5 +6,6 @@ exports.index = (req, res) => {
 
 exports.register = function(req, res) {
     const login = new Login(req.body);
-    res.send(login.body);
+    login.register();
+    res.send(login.errors);
 };
