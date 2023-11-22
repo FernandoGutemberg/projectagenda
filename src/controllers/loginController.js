1e33e98d7e75ab6c20f823dbc10ac2f276cbe374
FernandoGutemberg <fernando70sil@gmail.com>
2023-11-22 01:53:07 -0300

    criado usuário
    mensagem de usuário criado com sucesso

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,20 +1,28 @@
-const Login = require('../models/LoginModel');
-
-exports.index = (req, res) => {
-    res.render('login');
-};
-
-exports.register = async function(req, res) {
-    const login = new Login(req.body);
-    await login.register();
-
-    if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
-        req.session.save(function() {
-            return res.redirect('back');
+    const Login = require('../models/LoginModel');
+     
+    exports.index = (req, res) => {
+        res.render('login');
+    };
+     
+    exports.register = async function(req, res) { 
+        try {
+        const login = new Login(req.body);
+        await login.register();
+     
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+               return res.redirect('/login/index');
+            });
+            return;
+        }
+     
+        req.flash('success', 'Seu usuário foi criado com sucesso.');
+        req.session.save(function () {
+            return res.redirect('/login/index');
         });
-        return;
-    }
-
-    res.send(login.errors);
-};
+     }  catch(e) {
+        console.log(e);
+        return res.render('404');
+        }
+    };
