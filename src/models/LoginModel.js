1481039e1fa0dacefd89c232353f9587586d4fe1
FernandoGutemberg <fernando70sil@gmail.com>
2023-11-22 18:48:44 -0300

    mensagem 'usuario ja existe'
    Adicionando interação 'usuário ja existe'

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -19,11 +20,23 @@ class Login {
     this.valida();
     if(this.errors.length > 0) return;
 
+    await this.userExists();
+
+    if(this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
     try {
       this.user = await LoginModel.create(this.body);
     } catch(e) {
       console.log(e);
     }
+  }
+
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email });
+    if(user) this.errors.push('Usuário ja existe');
   }
 
   valida() {
