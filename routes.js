d2560a72d87bfaa54f510f54c02a8d9b28c6d53e
FernandoGutemberg <fernando70sil@gmail.com>
2023-11-23 00:35:52 -0300

    mensagens no login

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
-
 
 // Rotas da home
 route.get('/', homeController.index);
@@ -10,5 +10,6 @@ route.get('/', homeController.index);
 // Rotas de login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
 
 module.exports = route;
--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,28 +1,52 @@
-    const Login = require('../models/LoginModel');
-     
-    exports.index = (req, res) => {
-        res.render('login');
-    };
-     
-    exports.register = async function(req, res) { 
-        try {
-        const login = new Login(req.body);
-        await login.register();
-     
-        if(login.errors.length > 0) {
-            req.flash('errors', login.errors);
-            req.session.save(function () {
-               return res.redirect('/login/index');
-            });
-            return;
-        }
-     
-        req.flash('success', 'Seu usuário foi criado com sucesso.');
+const Login = require('../models/LoginModel');
+
+exports.index = (req, res) => {
+    res.render('login');
+};
+
+exports.register = async function(req, res) { 
+    try {
+    const login = new Login(req.body);
+    await login.register();
+
+    if(login.errors.length > 0) {
+        req.flash('errors', login.errors);
         req.session.save(function () {
-            return res.redirect('/login/index');
+        return res.redirect('/login/index');
         });
-     }  catch(e) {
-        console.log(e);
-        return res.render('404');
-        }
-    };
+        return;
+    }
+
+    req.flash('success', 'Seu usuário foi criado com sucesso.');
+    req.session.save(function() {
+        return res.redirect('/login/index');
+    });
+}  catch(e) {
+    console.log(e);
+    return res.render('404');
+    }
+};
+
+exports.login = async function(req, res) { 
+    try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if(login.errors.length > 0) {
+        req.flash('errors', login.errors);
+        req.session.save(function () {
+        return res.redirect('/login/index');
+        });
+        return;
+    }
+
+    req.flash('success', 'Você entrou no sistema.');
+    req.session.user = login.user;
+    req.session.save(function() {
+        return res.redirect('/login/index');
+    });
+}  catch(e) {
+    console.log(e);
+    return res.render('404');
+    }
+};
