16a6c00dfb054104db108e3dbe14a4b39afafe2d
FernandoGutemberg <fernando70sil@gmail.com>
2023-11-27 19:21:06 -0300

    cadastrando

--- routes.js
+++ routes.js
@@ -20,6 +20,8 @@ route.get('/login/logout', loginController.logout);
 // Rotas de contato
 route.get('/contato/index', loginRequired, contatoController.index);
 route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+
 
 
 module.exports = route;
--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,7 +1,9 @@
 const Contato = require('../models/ContatoModel');
 
 exports.index = (req, res) => {
-    res.render('contato');
+    res.render('contato', {
+        contato: {}
+    });
 };
 
 exports.register = async (req, res) => {
@@ -16,11 +18,19 @@ exports.register = async (req, res) => {
         }
     
         req.flash('success', 'Contato registrado com sucesso.');
-        req.session.save(() => res.redirect('back'));
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
         return;   
     } catch(e) {
-
         console.log(e);
         return res.render('404');
     }
+};
+
+exports.editIndex = async function(req, res) {
+    if(!req.params.id) return res.render('404');
+
+    const contato = await Contato.buscaPorId(req.params.id);
+    if(!contato) return res.render('404');
+    
+    res.render('contato', { contato });
 };
